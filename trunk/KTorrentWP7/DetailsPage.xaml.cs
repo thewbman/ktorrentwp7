7c888db79f3cc7109296fc6a24437e4794bf3c13
thewbman <thewbman@fad9404a-9d10-06a2-7bf1-edc1debab3d2>
2011-09-10 21:23:48 +0000

    a

---
+++ trunk/KTorrentWP7/DetailsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+namespace KTorrentWP7
+{
+    public partial class DetailsPage : PhoneApplicationPage
+    {
+        // Constructor
+        public DetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        private int hostIndex;
+
+        // When page is navigated to set data context to selected item in list
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string selectedIndex = "";
+            if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex))
+            {
+                hostIndex = int.Parse(selectedIndex);
+                DataContext = App.ViewModel.Hosts[hostIndex];
+            }
+        }
+
+
+    }
+}
